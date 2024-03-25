f7296751c0ebd70ecf5612fcd1a770d94e382458
Gabija <gabijagavronskyte@gmail.com>
2024-03-25 21:15:03 +0200

    Written Unit tests

--- app/src/main/java/com/example/wordcounter/TextCounter.java
+++ app/src/main/java/com/example/wordcounter/TextCounter.java
@@ -1,11 +1,24 @@
 package com.example.wordcounter;
 
 public class TextCounter {
-    public static int getCharsCount(String phrase){
-        return phrase.length();
+    public static int getCharsCount(String phrase) {
+        if (phrase == null) {
+            return 0;
+        }
+        return phrase.replaceAll("\\s", "").length();
     }
-    public static int getWordsCount(String phrase){
-        return phrase.split("\\s+").length;
+    public static int getWordsCount(String phrase) {
+        if (phrase == null) {
+            return 0;
+        }
+        String[] words = phrase.split("\\s+|\\p{Punct}");
+        int count = 0;
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
--- app/src/test/java/com/example/wordcounter/ExampleUnitTest.java
+++ app/src/test/java/com/example/wordcounter/ExampleUnitTest.java
@@ -11,7 +11,94 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Given_EmptyText_getCharsCount_ZeroReturned() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_EmptyText_getWordsCount_ZeroReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_null_getCharsCount_ZeroReturned() {
+        String givenString = null;
+        int expectedValue = 0;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_null_getWordsCount_ZeroReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = null;
+        int expectedValue = 0;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_Space_getCharsCount_ZeroReturned() {
+        String givenString = "  ";
+        int expectedValue = 0;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_Space_getWordsCount_ZeroReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "  ";
+        int expectedValue = 0;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_NormalText_getCharsCount_CountReturned() {
+        String givenString = "Labas vakaras";
+        int expectedValue = 12;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_NormalText_getWordsCount_CountReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "Labas vakaras";
+        int expectedValue = 2;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_Equation_getCharsCount_CharsReturned() {
+        String givenString = "2+2=4";
+        int expectedValue = 5;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_Equation_getWordsCount_NumbersReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "2+2=4";
+        int expectedValue = 3;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_OnlyComma_getCharsCount_CharsReturned() {
+        String givenString = ",,, ,,,";
+        int expectedValue = 6;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_OnlyComma_getWordsCount_ZeroReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = ",,, ,,,";
+        int expectedValue = 0;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+
     }
 }
